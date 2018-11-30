ce6ecfc45d911476c87f7d51223ff635bd4b6d72
dedaloc2 <joganel@hotmail.com>
2018-11-30 13:50:42 -0600

    Adding index page

---
+++ src/main/java/com/review/review3/Review3Application.java
@@ -0,0 +1,12 @@
+package com.review.review3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Review3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Review3Application.class, args);
+    }
+}
